96d1ea590bf6d7366c8dc13ab99502a1c61b0401
ArturIdiyatullin <31572787+ArturIdiyatullin@users.noreply.github.com>
2017-12-22 14:34:12 +0300

    Update calcc.c

--- calcc.c
+++ calcc.c
@@ -1,55 +1,141 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> /* for atof() */
+#include <math.h> /* for fmod() */
 
-int *stack; /*Create a stack*/
-int sp = 0;
+#define MAXOP 100 /* max size of operand or operator */
+#define NUMBER '0' /* the sign of the number */
 
-int pop(void){ /*Remove the element from the top of the stack by moving the vertex to the next element*/
-if (sp > 0){
-return stack[--sp];
-}
-else {
-printf("-1");
-return 0;
-}
-}
+int getop (char []);
+void push (double);
+double pop (void);
 
-void push(int a){ /*Put the element on top of the stack*/
-stack[sp++] = a;
-}
-
-int main(int argc, char *argv[])
+/* a calculator with reverse Polish entry , idiatullin*/
+int main()
 {
-int k, i;
-k = argc;
+int type;
+double op1, op2;
+char s[MAXOP];
 
-stack =(int *) malloc ( sizeof(int) * k ); /*Selecting memory*/
-if ( stack == NULL ) exit (1);
-
-for( i = 1; i < k; i++ )
-{
-switch ( (char) argv[i][0] ){ /*Collating operation type*/
-case '\n':
-break;
-case '=':
-printf("\nResult = %d\n", pop());
+while ((type = getop (s)) != EOF) {
+switch (type) {
+case NUMBER:
+push (atof(s));
 break;
 case '+':
-push(pop() + pop());
+push (pop() + pop());
+break;
+case '*':
+push (pop() * pop());
 break;
 case '-':
-push(-pop() + pop());
-break;
-case 'x':
-push(pop() * pop());
+op2 = pop();
+push (pop() - op2);
 break;
 case '/':
-push(pop() / pop());
+op2 = pop();
+if (op2 != 0.0)
+push (pop() / op2);
+else
+printf("error: division by zero\n");
+break;
+case '%':
+op2 = pop();
+if (op2 != 0.0) {
+op1 = pop();
+push ( op1 - op2 * floor(op1 / op2) );
+}
+else
+printf("error: division by zero\n");
+break;
+case '\n':
+printf("\t%.8g\n", pop());
 break;
 default:
-push( atoi(argv[i]));
+printf("error: unknown operation %s\n", s);
 break;
 }
 }
+
 return 0;
+}
+
+
+#define MAXVAL 100 /* the maximum depth of the stack */
+
+int sp = 0; /* next free position in the stack */
+double val[MAXVAL]; /* stack */
+
+/* push: to put the value of F on the stack */
+void push(double f)
+{
+if (sp < MAXVAL)
+val[sp++] = f;
+else
+printf("error: stack is full, %g does not fit\n", f);
+}
+
+/* pop: take the top of the stack and give as a result */
+double pop(void)
+{
+if (sp > 0)
+return val[--sp];
+else {
+printf ("error: stack is empty\n");
+return 0.0;
+}
+}
+
+
+#include <ctype.h>
+
+int getch(void);
+void ungetch(int);
+
+/* getop: gets the next operator or operand */
+int getop(char s[])
+{
+int i=0, c, t;
+while ((s[i] = c = getch()) == ' ' || c == '\t');
+if (!isdigit(c) && c != '.'){
+if (c == '-') {
+if (isdigit(t = getch()) || t == '.') {
+c = t;
+s[++i] = c;
+}
+else {
+ungetch(t);
+return c; /* not a number */
+}
+}
+else {
+return c; /* not a number */
+}
+}
+if (isdigit(c)) /* accumulate the integer part */
+while (isdigit(s[++i] = c = getch()));
+if (c =='.') /* accumulate the integer part */
+while (isdigit(s[++i] = c = getch()));
+s[i] = '\0';
+if (c != EOF)
+ungetch(c);
+return NUMBER;
+}
+
+
+#define BUFSIZE 100
+
+char buf[BUFSIZE]; /* buffer for ungetch */
+int bufp = 0; /* next free position in buffer , */
+
+int getch(void) /* to take (possibly returned) symbol */
+{
+return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c) /* to return the symbol to enter, idiatullin*/
+{
+if (bufp >= BUFSIZE)
+printf("ungetch: too many characters\n");
+else
+buf[bufp++] = c;
 }
